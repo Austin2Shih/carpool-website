6cb6c14c16ce029528017887b9a0e5c17532eb81
Austin Shih <austinhshih@gmail.com>
2022-04-16 22:42:12 -0700

    Fixed link

--- pages/nav.js
+++ pages/nav.js
@@ -1,7 +1,7 @@
 import { CgProfile } from "react-icons/cg"
 import { GrHomeRounded } from "react-icons/gr"
 import { ImBubble2 } from "react-icons/im"
-
+import Link from 'next/link'
 import styles from '../styles/nav.module.css'
 
 function Nav() {
@@ -10,9 +10,9 @@ function Nav() {
             <nav className={styles.navbar}>
                 <div className={styles.container}>
                     <ul className={styles.list}>
-                        <li><a className={styles.listItem} href='/chat'><ImBubble2 size={40} /></a></li>
-                        <li><a className={styles.listItem} href='/'><GrHomeRounded size={40} /></a></li>
-                        <li><a className={styles.listItem} href='/profile'><CgProfile size={40} /></a></li>
+                        <li><Link className={styles.listItem} href='/chat'><ImBubble2 size={40} /></Link></li>
+                        <li><Link className={styles.listItem} href='/'><GrHomeRounded size={40} /></Link></li>
+                        <li><Link className={styles.listItem} href='/profile'><CgProfile size={40} /></Link></li>
                     </ul>
                 </div>
             </nav>
